bc8d797683f2868711df26ecf433bc4d664347fb
Armonia Romero <romeroa@correo.unimet.edu.ve>
2023-02-04 13:16:05 -0400

    navbar

---
+++ index.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('nav');
+window.onscroll = () => {
+    if (window.pageYOffset > 0) {
+        nav.classList.add("sticky")
+    }
+    else {
+        nav.classList.remove("sticky");
+    }
+}
+let bar = document.querySelector('.menu');
+bar.onclick = () => {
+    bar.classList.toggle('active');
+    nav.classList.toggle("active");
+}
